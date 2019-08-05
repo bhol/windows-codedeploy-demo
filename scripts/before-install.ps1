9c5a6548e497fd1707d41d7c538f1fa0231ddf69
Brett Hollman <brett@getcloudier.com>
2019-08-05 13:52:08 -0700

    reverting to .bat file to install and deploy web server

--- scripts/before-install.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-echo "Currently executing custom powershell script for BeforeInstall hook"
-
-# Install IIS
-Import-Module ServerManager; 
-Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName 'IIS-WebServerRole', 'IIS-WebServer', 'IIS-ManagementConsole';
